bf5533e8d07a4ec4dc22eb25972cd79aed39b2f5
julienlucas <contact@julienlucas.com>
2022-09-22 14:44:39 +0200

    first commit

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,16 +1,53 @@
-import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+// import styled from 'styled-components';
+// import NxWelcome from './nx-welcome';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+// const StyledApp = styled.div`
+//   // Your style here
+// `;
 
-export function App() {
+// export function App() {
+//   return (
+//     <StyledApp>
+//       <NxWelcome title="todos" />
+//     </StyledApp>
+//   );
+// }
+
+// export default App;
+
+import { useEffect, useState } from 'react';
+import { Todo } from '@myorg/data';
+import { Todos } from '@myorg/ui';
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then((_) => _.json())
+      .then(setTodos);
+  }, []);
+
+  function addTodo() {
+    fetch('/api/addTodo', {
+      method: 'POST',
+      body: '',
+    })
+      .then((_) => _.json())
+      .then((newTodo) => {
+        setTodos([...todos, newTodo]);
+      });
+  }
+
   return (
-    <StyledApp>
-      <NxWelcome title="todos" />
-    </StyledApp>
+    <>
+      <h1>Todos</h1>
+      <Todos todos={todos} />
+      <button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </button>
+    </>
   );
-}
+};
 
 export default App;
